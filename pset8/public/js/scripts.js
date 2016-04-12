093e8eddee177de38aa43efe1885ec0f729ec623
Roman I. Burdakov <roman.burdakov@harvardbusiness.org>
2016-04-12 18:15:24 +0000

    Solution for pset8

--- pset8/public/js/scripts.js
+++ pset8/public/js/scripts.js
@@ -21,51 +21,51 @@ var info = new google.maps.InfoWindow();
 // execute when the DOM is fully loaded
 $(function() {
 
-    // styles for map
-    // https://developers.google.com/maps/documentation/javascript/styling
-    var styles = [
+  // styles for map
+  // https://developers.google.com/maps/documentation/javascript/styling
+  var styles = [
 
-        // hide Google's labels
-        {
-            featureType: "all",
-            elementType: "labels",
-            stylers: [
-                {visibility: "off"}
-            ]
-        },
+    // hide Google's labels
+    {
+      featureType: "all",
+      elementType: "labels",
+      stylers: [
+        {visibility: "off"}
+      ]
+    },
 
-        // hide roads
-        {
-            featureType: "road",
-            elementType: "geometry",
-            stylers: [
-                {visibility: "off"}
-            ]
-        }
+    // hide roads
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        {visibility: "off"}
+      ]
+    }
 
-    ];
+  ];
 
-    // options for map
-    // https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var options = {
-        center: {lat: 37.4236, lng: -122.1619}, // Stanford, California
-        disableDefaultUI: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        maxZoom: 14,
-        panControl: true,
-        styles: styles,
-        zoom: 13,
-        zoomControl: true
-    };
+  // options for map
+  // https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var options = {
+    center: {lat: 42.3770, lng: -71.1256}, // Cambridge, Massachussets
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    maxZoom: 14,
+    panControl: true,
+    styles: styles,
+    zoom: 13,
+    zoomControl: true
+  };
 
-    // get DOM node in which map will be instantiated
-    var canvas = $("#map-canvas").get(0);
+  // get DOM node in which map will be instantiated
+  var canvas = $("#map-canvas").get(0);
 
-    // instantiate map
-    map = new google.maps.Map(canvas, options);
+  // instantiate map
+  map = new google.maps.Map(canvas, options);
 
-    // configure UI once Google Map is idle (i.e., loaded)
-    google.maps.event.addListenerOnce(map, "idle", configure);
+  // configure UI once Google Map is idle (i.e., loaded)
+  google.maps.event.addListenerOnce(map, "idle", configure);
 
 });
 
@@ -74,7 +74,55 @@ $(function() {
  */
 function addMarker(place)
 {
-    // TODO
+  if (place == null) {
+    console.log("Wrong place object was passed");
+  }
+  // find out position on the map for the given coordinates
+  var position = new google.maps.LatLng(place.latitude, place.longitude);
+  // icon
+  var pIcon = new google.maps.MarkerImage('img/loc_marker.png', null, null, null,
+      new google.maps.Size(30, 30));
+  // build marker with labels
+  var marker = new MarkerWithLabel({
+    position: position,
+    icon: pIcon,
+    map: map,
+    labelContent: place.place_name,
+    labelAnchor: new google.maps.Point(22, 0),
+    labelClass: "labels", // the CSS class for the label
+    labelStyle: {opacity: 0.75}
+  });
+
+  // adding click event listener
+  marker.addListener('click', function() {
+    var parameters = {
+      geo: place.place_name + "+" + place.postal_code + "+" + place.admin_name1
+    };
+
+    $.getJSON("articles.php", parameters)
+        .done(function(data, textStatus, jqXHR) {
+          // _ - is an underscore library with help functions.
+          if (_.isEmpty(data)) {
+            content = "<p>There are no news for the given area.</p>";
+          } else {
+            // list articles for the given area
+            var articles = "<ul>";
+            _.each(data, function(article) {
+              articles += "<li><a href=" + article.link + "/>"+ article.title + "</a></li>";
+            });
+            articles += "</ul>";
+          }
+
+          // call to display resutls
+          showInfo(marker, articles);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          // log error to browser's console
+          console.log(errorThrown.toString());
+        });
+  });
+  // register marker
+  markers.push(marker);
 }
 
 /**
@@ -82,68 +130,68 @@ function addMarker(place)
  */
 function configure()
 {
-    // update UI after map has been dragged
-    google.maps.event.addListener(map, "dragend", function() {
-        update();
-    });
+  // update UI after map has been dragged
+  google.maps.event.addListener(map, "dragend", function() {
+    update();
+  });
 
-    // update UI after zoom level changes
-    google.maps.event.addListener(map, "zoom_changed", function() {
-        update();
-    });
+  // update UI after zoom level changes
+  google.maps.event.addListener(map, "zoom_changed", function() {
+    update();
+  });
 
-    // remove markers whilst dragging
-    google.maps.event.addListener(map, "dragstart", function() {
-        removeMarkers();
-    });
+  // remove markers whilst dragging
+  google.maps.event.addListener(map, "dragstart", function() {
+    removeMarkers();
+  });
 
-    // configure typeahead
-    // https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md
-    $("#q").typeahead({
+  // configure typeahead
+  // https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md
+  $("#q").typeahead({
         autoselect: true,
         highlight: true,
         minLength: 1
-    },
-    {
+      },
+      {
         source: search,
         templates: {
-            empty: "no places found yet",
-            suggestion: _.template("<p>TODO</p>")
+          empty: "no places found yet",
+          suggestion: _.template("<p><%- place_name %>, <%- admin_name1 %></p>")
         }
-    });
+      });
 
-    // re-center map after place is selected from drop-down
-    $("#q").on("typeahead:selected", function(eventObject, suggestion, name) {
+  // re-center map after place is selected from drop-down
+  $("#q").on("typeahead:selected", function(eventObject, suggestion, name) {
 
-        // ensure coordinates are numbers
-        var latitude = (_.isNumber(suggestion.latitude)) ? suggestion.latitude : parseFloat(suggestion.latitude);
-        var longitude = (_.isNumber(suggestion.longitude)) ? suggestion.longitude : parseFloat(suggestion.longitude);
+    // ensure coordinates are numbers
+    var latitude = (_.isNumber(suggestion.latitude)) ? suggestion.latitude : parseFloat(suggestion.latitude);
+    var longitude = (_.isNumber(suggestion.longitude)) ? suggestion.longitude : parseFloat(suggestion.longitude);
 
-        // set map's center
-        map.setCenter({lat: latitude, lng: longitude});
-
-        // update UI
-        update();
-    });
-
-    // hide info window when text box has focus
-    $("#q").focus(function(eventData) {
-        hideInfo();
-    });
-
-    // re-enable ctrl- and right-clicking (and thus Inspect Element) on Google Map
-    // https://chrome.google.com/webstore/detail/allow-right-click/hompjdfbfmmmgflfjdlnkohcplmboaeo?hl=en
-    document.addEventListener("contextmenu", function(event) {
-        event.returnValue = true; 
-        event.stopPropagation && event.stopPropagation(); 
-        event.cancelBubble && event.cancelBubble();
-    }, true);
+    // set map's center
+    map.setCenter({lat: latitude, lng: longitude});
 
     // update UI
     update();
+  });
 
-    // give focus to text box
-    $("#q").focus();
+  // hide info window when text box has focus
+  $("#q").focus(function(eventData) {
+    hideInfo();
+  });
+
+  // re-enable ctrl- and right-clicking (and thus Inspect Element) on Google Map
+  // https://chrome.google.com/webstore/detail/allow-right-click/hompjdfbfmmmgflfjdlnkohcplmboaeo?hl=en
+  document.addEventListener("contextmenu", function(event) {
+    event.returnValue = true;
+    event.stopPropagation && event.stopPropagation();
+    event.cancelBubble && event.cancelBubble();
+  }, true);
+
+  // update UI
+  update();
+
+  // give focus to text box
+  $("#q").focus();
 }
 
 /**
@@ -151,7 +199,7 @@ function configure()
  */
 function hideInfo()
 {
-    info.close();
+  info.close();
 }
 
 /**
@@ -159,7 +207,11 @@ function hideInfo()
  */
 function removeMarkers()
 {
-    // TODO
+  // clean up array of markers
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
 }
 
 /**
@@ -167,21 +219,21 @@ function removeMarkers()
  */
 function search(query, cb)
 {
-    // get places matching query (asynchronously)
-    var parameters = {
-        geo: query
-    };
-    $.getJSON("search.php", parameters)
-    .done(function(data, textStatus, jqXHR) {
+  // get places matching query (asynchronously)
+  var parameters = {
+    geo: query
+  };
+  $.getJSON("search.php", parameters)
+      .done(function(data, textStatus, jqXHR) {
 
         // call typeahead's callback with search results (i.e., places)
         cb(data);
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
 
         // log error to browser's console
         console.log(errorThrown.toString());
-    });
+      });
 }
 
 /**
@@ -189,46 +241,46 @@ function search(query, cb)
  */
 function showInfo(marker, content)
 {
-    // start div
-    var div = "<div id='info'>";
-    if (typeof(content) === "undefined")
-    {
-        // http://www.ajaxload.info/
-        div += "<img alt='loading' src='img/ajax-loader.gif'/>";
-    }
-    else
-    {
-        div += content;
-    }
+  // start div
+  var div = "<div id='info'>";
+  if (typeof(content) === "undefined")
+  {
+    // http://www.ajaxload.info/
+    div += "<img alt='loading' src='img/ajax-loader.gif'/>";
+  }
+  else
+  {
+    div += content;
+  }
 
-    // end div
-    div += "</div>";
+  // end div
+  div += "</div>";
 
-    // set info window's content
-    info.setContent(div);
+  // set info window's content
+  info.setContent(div);
 
-    // open info window (if not already open)
-    info.open(map, marker);
+  // open info window (if not already open)
+  info.open(map, marker);
 }
 
 /**
  * Updates UI's markers.
  */
-function update() 
+function update()
 {
-    // get map's bounds
-    var bounds = map.getBounds();
-    var ne = bounds.getNorthEast();
-    var sw = bounds.getSouthWest();
+  // get map's bounds
+  var bounds = map.getBounds();
+  var ne = bounds.getNorthEast();
+  var sw = bounds.getSouthWest();
 
-    // get places within bounds (asynchronously)
-    var parameters = {
-        ne: ne.lat() + "," + ne.lng(),
-        q: $("#q").val(),
-        sw: sw.lat() + "," + sw.lng()
-    };
-    $.getJSON("update.php", parameters)
-    .done(function(data, textStatus, jqXHR) {
+  // get places within bounds (asynchronously)
+  var parameters = {
+    ne: ne.lat() + "," + ne.lng(),
+    q: $("#q").val(),
+    sw: sw.lat() + "," + sw.lng()
+  };
+  $.getJSON("update.php", parameters)
+      .done(function(data, textStatus, jqXHR) {
 
         // remove old markers from map
         removeMarkers();
@@ -236,12 +288,12 @@ function update()
         // add new markers to map
         for (var i = 0; i < data.length; i++)
         {
-            addMarker(data[i]);
+          addMarker(data[i]);
         }
-     })
-     .fail(function(jqXHR, textStatus, errorThrown) {
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
 
-         // log error to browser's console
-         console.log(errorThrown.toString());
-     });
+        // log error to browser's console
+        console.log(errorThrown.toString());
+      });
 }
